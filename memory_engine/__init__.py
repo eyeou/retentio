153dd2b4bcd48155f89667083c94f927ebaf2dcb
gaiaflaviamezaib <gaiaflaviamezaib@gmail.com>
2026-05-16 14:07:48 +0200

    Remove root memory package after Gaia move

--- memory_engine/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Memory reconstruction engine for Retentio."""
-
-from memory_engine.models import MemoryTrace, ReconstructedMemory
-from memory_engine.reconstructor import reconstruct_memory
-
-__all__ = ["MemoryTrace", "ReconstructedMemory", "reconstruct_memory"]
